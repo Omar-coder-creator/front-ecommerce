c902819c935c91476b8926690d3340b1927c8e45
omar <benghorbelomar5@gmail.com>
2025-03-29 15:30:20 +0100

    update

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,13 @@
-import { Link } from "react-router";
+import { useNavigate } from "react-router";
 const Home = () => {
+  const nav = useNavigate()
   return (
     <div>
       <section className="hero">
         <div className="hero-content">
           <h1>Welcome to ShopMate</h1>
           <p>Your one-stop shop for the latest trends and exclusive offers.</p>
-          <Link to='/products'>Shop now</Link>
+          <button onClick={()=>nav('/products')} id="shop-now">Shop now</button>
         </div>
       </section>
 
